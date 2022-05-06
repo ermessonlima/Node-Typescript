911316255091ae0f30a13dbfd52419da96658aaf
Ermesson Lima <ermessonlimadossantos@hotmail.com>
2022-05-05 22:57:15 -0300

    feat: create template node and typescript

---
+++ src/routes.ts
@@ -0,0 +1,6 @@
+import { Request, Response } from "express";
+import CreateCourseService from "./CreateCourseServices";
+
+export function createCourse(request: Request, response: Response) {
+  CreateCourseService.execute({ name: "Node", educator: "Ermesson" });
+}
---
+++ src/server.ts
@@ -0,0 +1,12 @@
+import express from 'express';
+import { createCourse } from './routes';
+
+const app = express();
+
+app.get('/', createCourse );
+
+
+
+app.listen(3333, () => {
+    console.log('Server is running on port 3000');
+})
